84985afd39f259b41341eb11876ee1fb1cc28098
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2022-06-08 07:41:07 +0530

    while true validator

---
+++ src/main/java/org/quiltmc/javacodegen/validation/NoInfiniteLoopValidator.java
@@ -0,0 +1,16 @@
+package org.quiltmc.javacodegen.validation;
+
+import java.util.regex.Pattern;
+
+public enum NoInfiniteLoopValidator implements FileValidator.OfText {
+	INSTANCE;
+
+	static final Pattern WHILETRUE_PATTERN = Pattern.compile("while\\s?\\(true\\)");
+
+	@Override
+	public void validate(String text) {
+		if(WHILETRUE_PATTERN.matcher(text).find()) {
+			throw new IllegalStateException("An infinite loop was found");
+		}
+	}
+}
